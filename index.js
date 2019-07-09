570cd385c35c941863e02f45050792f0ad3dad68
Tom White <tomjameswhite94@gmail.com>
2019-07-09 12:25:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function happyHolidays() {
   return("Happy holidays!");
 }
-happyHolidaysTo('you');
+happyHolidaysTo(you)
 function happyHolidaysTo(name) {
   return("Happy holidays, ${name}!");
 }
