fb5f73b872f59ba17981de837512cdde0189e19f
Tom White <tomjameswhite94@gmail.com>
2019-07-09 12:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-function happyHolidays() {
-  return("Happy holidays!");
+function happyHolidays(greetings){
+  return greetings;
 }
+happyHolidays("Happy holidays!")
 
 function happyHolidaysTo(name) {
-  return("Happy holidays, ${name}!");
-  }
+  
+  return `Happy holidays, ${name}!`;
+}
+happyHolidaysTo (`Alex`)
 
 function happyHolidayTo(holiday, name) {
-  return("Happy ${holiday}, ${name}!");
+  return `Happy ${holiday}, ${name}!`;
 }
+happyHolidayTo(`Christmass`, `Alex`)
 
 function holidayCountdown(days, holiday) {
-  return("It's ${days} days until ${holiday}!");
+    return `It's ${days} days until ${holiday}!`;
 }
+holidayCountdown(12, `Yom Khuledet`)
