e54465e1769d15ecbc5444d2b90f11eb8bdd7a1e
Tom White <tomjameswhite94@gmail.com>
2019-07-09 11:56:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function happyHolidays(String)
-return ()
+function happyHolidays(String) {
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo(string) {
+  console.log("Happy holidays, ${name}!")
+}
+function happyHolidayTo(string) {
+  console.log("Happy ${holiday}, ${name}!")
+}
+function holidayCountdown(string) {
+  console.log("It's ${days} days until ${holiday}!")
+}
