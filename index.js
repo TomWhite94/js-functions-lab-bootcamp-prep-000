50959d05c91ce3b2815fa6e44b63e0f9255f5af5
Tom White <tomjameswhite94@gmail.com>
2019-07-09 12:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 function happyHolidays() {
   return("Happy holidays!");
 }
+
 function happyHolidaysTo(name) {
   return("Happy holidays, ${name}!")
   }
 happyHolidaysTo('you')
+
 function happyHolidayTo(holiday, name) {
   return("Happy ${holiday}, ${name}!");
 }
+
 function holidayCountdown(days, holiday) {
   return("It's ${days} days until ${holiday}!");
 }
